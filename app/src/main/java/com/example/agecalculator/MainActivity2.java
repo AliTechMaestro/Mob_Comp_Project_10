67fb23747ca000146181899cc18ea425506507dc
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-22 13:45:27 +0500

    User Interface (Commit 4 Done)

--- app/src/main/java/com/example/agecalculator/MainActivity2.java
+++ app/src/main/java/com/example/agecalculator/MainActivity2.java
@@ -22,9 +22,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         editN = findViewById(R.id.editName);
