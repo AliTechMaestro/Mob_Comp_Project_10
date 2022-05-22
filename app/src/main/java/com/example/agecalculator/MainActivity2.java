db46a18a2f01ea5667896e051cafa5ba963a1837
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-22 13:39:49 +0500

    Age Displayed with Name (Commit 3 Done)

--- app/src/main/java/com/example/agecalculator/MainActivity2.java
+++ app/src/main/java/com/example/agecalculator/MainActivity2.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 
@@ -16,8 +17,14 @@ public class MainActivity2 extends AppCompatActivity {
     EditText editN,editY;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         editN = findViewById(R.id.editName);
--- app/src/main/java/com/example/agecalculator/MainActivity3.java
+++ app/src/main/java/com/example/agecalculator/MainActivity3.java
@@ -2,13 +2,35 @@ package com.example.agecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+    TextView textN,textA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        textN = findViewById(R.id.textName);
+        textA = findViewById(R.id.textAge);
+
+        Intent intent = getIntent();
+        String age = String.valueOf(intent.getIntExtra("Age",0));
+
+        String name = intent.getStringExtra("Name");
+
+        textN.setText(name);
+        textA.setText("Age: "+age);
+
     }
+
+    public void GoBack(View v){
+        Intent intent = new Intent(MainActivity3.this,MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
